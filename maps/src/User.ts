1258679c0ee044ff6ec9be2f4068c524b91b436c
Filip Burakowski <filip.burakowski@gmail.com>
2021-08-24 21:06:50 +0200

    Work on stats project

--- maps/src/User.ts
+++ maps/src/User.ts
@@ -2,21 +2,20 @@ import faker from 'faker';
 import { Mappable } from './CustomMap';
 
 export class User implements Mappable {
-    constructor() {
-        this.name = faker.name.firstName();
-        this.location = {
-            lng: parseFloat(faker.address.longitude()),
-            lat: parseFloat(faker.address.latitude()),
-
-        }
-    }
-    name: string;
-    location: {
-        lat: number;
-        lng: number;
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lng: parseFloat(faker.address.longitude()),
+      lat: parseFloat(faker.address.latitude()),
     };
+  }
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
 
-    markerContent(): string {
-        return `User Name ${this.name}`;
-    }
+  markerContent(): string {
+    return `User Name ${this.name}`;
+  }
 }
