3f8143011c6727c0c0753f9a858993434d010c90
Filip Burakowski <filip.burakowski@gmail.com>
2021-09-05 21:43:59 +0200

    Refactor part 1

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,8 +1,10 @@
-import { CsvFileReader } from './CsvFileReader';
+import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
 
-const reader = new CsvFileReader('football.csv');
+const reader = new MatchReader('football.csv');
 reader.read();
+
+const dateOfFirstMatch = reader.data[0][0];
 
 let manUnitedWins = 0;
 
@@ -14,3 +16,4 @@ for (let match of reader.data) {
   }
 }
 console.log(`Man United won ${manUnitedWins} games`);
+console.log(`First match is on ${dateOfFirstMatch}`);
