c6ee1d9fcafb0abd25be35841c8a3a8f8627f05e
Filip Burakowski <filip.burakowski@gmail.com>
2021-10-03 14:21:48 +0200

    Finish the stats project

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,19 +1,8 @@
 import { MatchReader } from './MatchReader';
-import { MatchResult } from './MatchResult';
+import { Summary } from './Summary';
 
-const reader = new MatchReader('football.csv');
-reader.read();
+const matchReader = MatchReader.fromCsv('football.csv');
+const summary = Summary.winsAnalisysWithHtmlReport('Man United');
 
-const dateOfFirstMatch = reader.data[0][0];
-
-let manUnitedWins = 0;
-
-for (let match of reader.data) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-console.log(`Man United won ${manUnitedWins} games`);
-console.log(`First match is on ${dateOfFirstMatch}`);
+matchReader.load();
+summary.buildAndPrintReport(matchReader.matches);
